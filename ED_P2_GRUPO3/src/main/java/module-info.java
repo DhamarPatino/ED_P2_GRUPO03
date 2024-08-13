70d12e050cd1759f1dbc987a7836e477c2e833fe
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-08-13 17:35:24 -0500

    Proyecto creado

---
+++ ED_P2_GRUPO3/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ec.edu.espol.ed_p2_grupo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens ec.edu.espol.ed_p2_grupo3 to javafx.fxml;
+    exports ec.edu.espol.ed_p2_grupo3;
+}
